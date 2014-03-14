f8527aa285c8c333faa41f196a88cd747726eb16
Ben Jeffrey <mail@benjeffrey.com>
2014-03-14 13:48:17 +0000

    Rename, fix Nginx configuration to redirect https://www. correctly.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 # sync compiled HTML
 rsync -aze ssh ./_site/ parsley:/var/www/benjeffrey.com/
 # sync Nginx configuration
-rsync -aze ssh ./nginx parsley:/etc/nginx/sites_enabled/benjeffrey.com
+rsync -aze ssh ./nginx.conf parsley:/etc/nginx/sites_enabled/benjeffrey.com
